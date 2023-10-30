940cf5805bf7ebde4f69b894d323ee40399293cf
dtmoyaji <daianji@gmail.com>
2023-10-30 17:50:28 +0900

    TableStructureを作成し、Groovyクラスのスケルトンを生成できるようにした。

--- src/main/java/org/tiny/customtable/editor/TableStructureEditor.java
+++ src/main/java/org/tiny/customtable/editor/TableStructureEditor.java
@@ -1,6 +1,7 @@
 
 package org.tiny.customtable.editor;
 
+import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.panel.Panel;
 
 
@@ -10,8 +11,13 @@ import org.apache.wicket.markup.html.panel.Panel;
  */
 public class TableStructureEditor extends Panel{
     
+    private Form tableStructureEditorForm;
+    
     public TableStructureEditor(String id){
         super(id);
+        
+        this.tableStructureEditorForm=new Form("tableStructureEditorForm");
+        this.add(this.tableStructureEditorForm);
     }
 
 }
